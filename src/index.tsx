c54796bd472bd1f390f3edffc6c2ec7b35792875
sirooni <82398008+sirooni@users.noreply.github.com>
2024-07-09 03:33:13 +0900

    chore: tailwindcssの導入

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Hello } from './pages/Hello'
 
+const Page: FC<{}> = () => {
+	return (
+		<div className='bg-slate-500'>
+			<Hello />
+		</div>
+	)
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<Hello />)
