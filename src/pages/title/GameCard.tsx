b2ac77d7b08af1642527217a8ee75c38ebce105a
sirooni <82398008+sirooni@users.noreply.github.com>
2024-12-15 17:28:50 +0900

    feature: サムネイル

--- src/pages/title/GameCard.tsx
+++ src/pages/title/GameCard.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 type Props = {
-  src: string
+  thumnail: React.ReactNode
   title: string
   description: string
   link: string
 }
 
 export const GameCard: React.FC<Props> = ({
-  src,
+  thumnail,
   title,
   description,
   link,
@@ -17,12 +16,9 @@ export const GameCard: React.FC<Props> = ({
   return (
     <Link to={link}>
       <div className='border-[2px] border-main rounded-md bg-foreground hover:bg-accent cursor-pointer p-4 flex gap-10'>
-        <img
-          src={src}
-          width='128px'
-          height='128px'
-          className='flex-none bg-gray-100'
-        />
+        <div className='flex-none size-32 flex items-center justify-center'>
+          {thumnail}
+        </div>
         <div className='text-main flex flex-col gap-4'>
           <p className='text-xl'>{title}</p>
           <p className='text-sm whitespace-pre-wrap'>{description}</p>
